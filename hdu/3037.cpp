f56ba0c3c893c34a4d16148d729b91acecb41401
cen5bin <cen5bin@163.com>
2014-08-01 16:46:25 +0800

    hdu 3037

--- hdu/3037.cpp
+++ hdu/3037.cpp
@@ -3,35 +3,56 @@
 
 typedef long long LL;
 
-LL extend_euclid(LL a, LL b, LL &x, LL &y)
+LL fac[100010];  //n! % p的结果
+
+void init(LL p)
 {
-	if (b == 0)
-	{
-		x = 1;
-		y = 0;
-		return a;
-	}
-	LL ret = extend_euclid(b, a%b, x, y);
-	LL tmp = x;
-	x = y;
-	y = tmp - a / b * y;
-	return ret;
+	fac[0] = 1;
+	for (int i = 1; i <= p; i++)
+		fac[i] = fac[i-1] * i % p;
 }
 
-LL reverse_element(LL n, LL mod)
+LL exp_mod(LL a, LL b, LL c)
 {
-	LL x,y;
-	extend_euclid(n, mod, x, y);
-	return (x % mod + mod) % mod;
+	LL ans = 1;
+	a %= c;
+	while (b)
+	{
+		if (b&1)
+			ans = ans * a % c;
+		a = a * a % c;
+		b>>=1;
+	}
+	return ans;
 }
+
+//组合数计算, C(n,m) 模 mod的结果
+LL C(LL n, LL m, LL mod)
+{
+	if (m > n) return 0;
+	return fac[n] * exp_mod(fac[n-m]*fac[m]%mod, mod-2, mod) % mod;
+}
+
+LL lucas(LL n, LL m, LL mod)
+{
+	if (m==0) return 1;
+	LL tmp = C(n % mod, m % mod, mod);
+	if (tmp == 0) return 0;
+	return tmp * lucas(n/mod, m/mod, mod) % mod;
+}
+
 
 int main()
 {
+	
 	int t;
 	scanf("%d", &t);
-	while (t--) 
+	while (t--)
 	{
-
+		LL m,n,p;
+		scanf("%lld%lld%lld", &n, &m, &p);
+		init(p);
+		printf("%lld\n", lucas(m+n, n, p));
 	}
 	return 0;
 }
